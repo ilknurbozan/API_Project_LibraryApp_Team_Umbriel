fe18d2f3cebe64a0192b78e312eb77b0cc3891e6
ilknurbozan <ilknurbozan23@gmail.com>
2023-04-12 21:41:48 -0400

    initial commit

--- src/test/java/com/library/steps/US01_StepDefs.java
+++ src/test/java/com/library/steps/US01_StepDefs.java
@@ -19,27 +19,27 @@ public class US01_StepDefs {
 
 
     @Given("I logged Library api as a {string}")
-    public void i_logged_library_api_as_a(String string) {
+    public void i_logged_library_api_as_a(String userType) {
 
     }
     @Given("Accept header is {string}")
-    public void accept_header_is(String string) {
+    public void accept_header_is(String contentType) {
 
     }
     @When("I send GET request to {string} endpoint")
-    public void i_send_get_request_to_endpoint(String string) {
+    public void i_send_get_request_to_endpoint(String endpoint) {
 
     }
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer int1) {
+    public void status_code_should_be(Integer statusCode) {
 
     }
     @Then("Response Content type is {string}")
-    public void response_content_type_is(String string) {
+    public void response_content_type_is(String contentType) {
 
     }
     @Then("{string} field should not be null")
-    public void field_should_not_be_null(String string) {
+    public void field_should_not_be_null(String field) {
 
     }
 
