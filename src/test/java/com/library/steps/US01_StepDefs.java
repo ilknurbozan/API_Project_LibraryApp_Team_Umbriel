1d51f7ae71406fa49fe0ce9e1bd5cbb7945576dd
ilknurbozan <ilknurbozan23@gmail.com>
2023-04-12 21:36:15 -0400

    initial commit

--- src/test/java/com/library/steps/US01_StepDefs.java
+++ src/test/java/com/library/steps/US01_StepDefs.java
@@ -20,33 +20,27 @@ public class US01_StepDefs {
 
     @Given("I logged Library api as a {string}")
     public void i_logged_library_api_as_a(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Given("Accept header is {string}")
     public void accept_header_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("I send GET request to {string} endpoint")
     public void i_send_get_request_to_endpoint(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("status code should be {int}")
     public void status_code_should_be(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("Response Content type is {string}")
     public void response_content_type_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("{string} field should not be null")
     public void field_should_not_be_null(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
